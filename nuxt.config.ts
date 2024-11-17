b503dd778105bb827e6101986cc2e0d3bdb30ec9
Kuschel_Swein <kuschelswein@gmail.com>
2024-11-17 12:36:43 +0100

    Finish attempted monaco fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
         css: {
             preprocessorOptions: {
                 scss: {
+                    silenceDeprecations: ['legacy-js-api'],
                     quietDeps: true,
                 },
             },
